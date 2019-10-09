a026d8326168f6204823bdf039a6a3abf5875d7f
Christian Kühnel <kuhnel@google.com>
2019-10-09 10:16:31 +0200

    First version of TLS configuration

--- kubernetes/reverse-proxy/basic.sh
+++ kubernetes/reverse-proxy/basic.sh
@@ -6,9 +6,13 @@ set -eux
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/mandatory.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/cloud-generic.yaml
 
-# install jetstack based on
-# https://www.digitalocean.com/community/tutorials/how-to-set-up-an-nginx-ingress-with-cert-manager-on-digitalocean-kubernetes
-kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.8/deploy/manifests/00-crds.yaml
+# install certmanager based on
+# http://docs.cert-manager.io/en/latest/getting-started/install/kubernetes.html
+
+kubectl create namespace cert-manager
 kubectl label namespace kube-system certmanager.k8s.io/disable-validation="true"
-helm repo add jetstack https://charts.jetstack.io
-helm install --name cert-manager --namespace kube-system jetstack/cert-manager --version v0.8.0
+kubectl create clusterrolebinding cluster-admin-binding \
+  --clusterrole=cluster-admin \
+  --user=$(gcloud config get-value core/account)
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v0.10.1/cert-manager.yaml
+
