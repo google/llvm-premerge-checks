7aa09fd26e9edad3184fd811094d6c7a1317ef2b
Christian Kühnel <kuhnel@google.com>
2019-10-21 22:06:47 +0200

    added configuration of enabled projects

--- scripts/run_cmake.sh
+++ scripts/run_cmake.sh
@@ -28,6 +28,7 @@ mkdir -p ${TARGET_DIR}
 
 set +e
 cmake -GNinja ../llvm -DCMAKE_BUILD_TYPE=Release -D LLVM_ENABLE_LLD=ON \
+    -D LLVM_ENABLE_PROJECTS="clang;clang-tools-extra;libcxx;libcxxabi;lld" \
 	-D LLVM_CCACHE_BUILD=ON -D LLVM_CCACHE_DIR="${CCACHE_PATH}" -D LLVM_CCACHE_MAXSIZE=20G \
     -D LLVM_ENABLE_ASSERTIONS=ON -DCMAKE_CXX_FLAGS=-gmlt \
     -DLLVM_LIT_ARGS="--xunit-xml-output ${WORKSPACE}/build/test-results.xml" 2>&1 | tee -a cmake-log.txt
