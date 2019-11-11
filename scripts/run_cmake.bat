b0346385176aea767ed66c8d9075a06c2866b2a3
Christian Kühnel <kuhnel@google.com>
2019-11-11 13:53:17 +0000

    cmake is in patch now

--- scripts/run_cmake.bat
+++ scripts/run_cmake.bat
@@ -17,7 +17,7 @@ cd build
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64
 
-"C:\Program Files\CMake\bin\cmake.exe" ..\llvm -G Ninja -DCMAKE_BUILD_TYPE=Release ^
+cmake.exe ..\llvm -G Ninja -DCMAKE_BUILD_TYPE=Release ^
     -D LLVM_ENABLE_PROJECTS="clang;clang-tools-extra;libcxx;libcxxabi;lld" ^
     -D LLVM_ENABLE_ASSERTIONS=ON ^
     -DLLVM_LIT_ARGS="-v --xunit-xml-output test-results.xml"  
