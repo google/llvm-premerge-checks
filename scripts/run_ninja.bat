630e6449c19af6c999ffacaf65a5774bfc87fb3c
Christian Kühnel <kuhnel@google.com>
2019-11-11 09:05:55 +0000

    hiding license in batch script runs

--- scripts/run_ninja.bat
+++ scripts/run_ninja.bat
@@ -1,16 +1,16 @@
-rem Copyright 2019 Google LLC
-rem
-rem Licensed under the the Apache License v2.0 with LLVM Exceptions (the "License");
-rem you may not use this file except in compliance with the License.
-rem You may obtain a copy of the License at
-rem
-rem     https://llvm.org/LICENSE.txt
-rem
-rem Unless required by applicable law or agreed to in writing, software
-rem distributed under the License is distributed on an "AS IS" BASIS,
-rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-rem See the License for the specific language governing permissions and
-rem limitations under the License.
+@rem Copyright 2019 Google LLC
+@rem
+@rem Licensed under the the Apache License v2.0 with LLVM Exceptions (the "License");
+@rem you may not use this file except in compliance with the License.
+@rem You may obtain a copy of the License at
+@rem
+@rem     https://llvm.org/LICENSE.txt
+@rem
+@rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed on an "AS IS" BASIS,
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
 
 cd build
 
