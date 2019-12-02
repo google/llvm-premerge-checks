bac89cc03a0bafe4d9d18d39d7795e78e46cf052
Christian Kühnel <kuhnel@google.com>
2019-12-02 14:43:46 +0100

    reduced scope of build job

--- scripts/run_cmake.ps1
+++ scripts/run_cmake.ps1
@@ -22,7 +22,7 @@ Invoke-CmdScript "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools
 
 Invoke-Call -ScriptBlock { 
     cmake.exe ..\llvm -G Ninja -DCMAKE_BUILD_TYPE=Release `
-        -D LLVM_ENABLE_PROJECTS="clang;clang-tools-extra;libcxx;libcxxabi;lld" `
+        -D LLVM_ENABLE_PROJECTS="clang;clang-tools-extra" `
         -D LLVM_ENABLE_ASSERTIONS=ON `
         -DLLVM_LIT_ARGS="-v --xunit-xml-output test-results.xml" `
         -D LLVM_ENABLE_DIA_SDK=OFF
