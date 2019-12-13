a695983f7690db8290df41ae400eb14cc50e2517
Christian Kühnel <kuhnel@google.com>
2019-12-13 16:20:09 +0000

    added tracing

--- scripts/git_checkout.ps1
+++ scripts/git_checkout.ps1
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+Set-PSDebug -Trace 1
 
 if (Test-Path -PathType Container .git){
     Write-Output "performing git pull..."
