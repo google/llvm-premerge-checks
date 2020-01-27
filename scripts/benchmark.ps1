ce5b4d462ad620ddecff7c48d88f856afeb5abf8
Christian Kühnel <kuhnel@google.com>
2020-01-27 15:43:32 +0000

    improved benchmark script
    We should probably do this in python so we can use it cross-platform...

--- scripts/benchmark.ps1
+++ scripts/benchmark.ps1
@@ -12,13 +12,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# The script will benchmark the windows build steps.
+# It can be used to optimize the build performance.
+
 param(
     [Parameter(Mandatory=$true)][string]$WORKDIR
 )
 
-Measure-Command { git clone --depth 1 https://github.com/llvm/llvm-project $WORKDIR }
+. ${PSScriptRoot}\common.ps1
+function timeit() {
+    param(
+      [scriptblock] $cmd
+    )
+    Write-Host "Running $cmd..."
+    $secs=Measure-Command @cmd | Select-Object -Property TotalSeconds
+    if ($lastexitcode -ne 0) {
+        Write-Error "$cmd failed!"
+    }
+    Write-Host "$cmd ran for $($secs.TotalSeconds)"
+    #TODO: make this an absolute path!
+    Add-Content benchmark.txt "$cmd : $($secs.TotalSeconds)"
+}
 
+Write-Host "Deleting work dir: $WORKDIR"
+& cmd /c rd /s/q $WORKDIR | Out-Null
+& cmd /c rd /s/q benchmark.txt | Out-Null
+timeit {git clone --depth 1 https://github.com/llvm/llvm-project $WORKDIR}
 Set-Location $WORKDIR
-Measure-Command {& $PSScriptRoot\run_cmake.ps1}
-Measure-Command {& $PSScriptRoot\run_ninja.ps1 all}
-Measure-Command {& $PSScriptRoot\run_ninja.ps1 check-all}
+timeit {git pull}
+
+timeit {& $PSScriptRoot\run_cmake.ps1}
+timeit {& $PSScriptRoot\run_ninja.ps1 all}
+timeit {& $PSScriptRoot\run_ninja.ps1 check-all}
+# re-run to get time for tests without compilation
+timeit {& $PSScriptRoot\run_ninja.ps1 check-all}
+timeit {& cmd /c rd/s/q $WORKDIR}
