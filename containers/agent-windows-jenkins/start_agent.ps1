69ebbaa3f78d6dbd923e3744cc5bf1a093d64822
Christian Kühnel <kuhnel@google.com>
2020-02-03 10:46:29 +0000

    moved temp to SSD

--- containers/agent-windows-jenkins/start_agent.ps1
+++ containers/agent-windows-jenkins/start_agent.ps1
@@ -17,10 +17,9 @@ $JENKINS_SERVER="jenkins.local"
 $AGENT_ROOT="C:\ws"
 
 # authenticate gsutil
-# gcloud auth activate-service-account --key-file C:\credentials\build-agent-results_key.json
 Write-Output "C:\credentials\build-agent-results_key.json`nllvm-premerge-checks`n`n" | gsutil config -e
 
-$env:TEMP="C:\TEMP"
+$env:TEMP="${$AGENT_ROOT}\TEMP"
 $env:TMP="${env:TEMP}"
 
 java -jar ${env:SWARM_PLUGIN_JAR} `
