98eb0f0818739034b8323e4ffa1a41ac416d2005
Christian Kühnel <kuhnel@google.com>
2020-03-20 09:48:27 +0100

    run_cmake.py working locally on Linux

---
+++ scripts/run_cmake.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+# Copyright 2020 Google LLC
+#
+# Licensed under the the Apache License v2.0 with LLVM Exceptions (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+from enum import Enum
+from git import Repo
+import os
+import platform
+import shutil
+import subprocess
+from typing import List, Dict
+import yaml
+from choose_projects import ChooseProjects
+
+
+class OperatingSystem(Enum):
+    Linux = 'linux'
+    Windows = 'windows'
+
+
+class Configuration:
+
+    def __init__(self, config_file_path: str):
+        with open(config_file_path) as config_file:
+            config = yaml.load(config_file, Loader=yaml.SafeLoader)
+        self._environment = config['environment']  # type: Dict[OperatingSystem, Dict[str, str]]
+        self.general_cmake_arguments = config['arguments']['general']  # type: List[str]
+        self._specific_cmake_arguments = config['arguments']  # type: Dict[OperatingSystem, List[str]]
+        self._default_projects = config['default_projects']  # type: Dict[OperatingSystem, str]
+        self.operating_system = self._detect_os()  # type: OperatingSystem
+
+    @property
+    def environment(self) -> Dict[str, str]:
+        return self._environment[self.operating_system.value]
+
+    @property
+    def specific_cmake_arguments(self) -> List[str]:
+        return self._specific_cmake_arguments[self.operating_system.value]
+
+    @property
+    def default_projects(self) -> str:
+        return self._default_projects[self.operating_system.value]
+
+    @staticmethod
+    def _detect_os() -> OperatingSystem:
+        """Detect the current operating system."""
+        if platform.system() == 'Windows':
+            return OperatingSystem.Windows
+        return OperatingSystem.Linux
+
+
+def _select_projects(config: Configuration, projects: str, repo_path: str) -> str:
+    """select which projects to build.
+
+    if projects == "default", a default configuraiton will be used.
+
+    if project == "detect", ChooseProjects is used to magically detect the projects
+         based on the files modified in HEAD
+    """
+    if projects == "default" or projects is None or len(projects) == 0:
+        return config.default_projects
+    if projects == "detect":
+        cp = ChooseProjects(repo_path)
+        repo = Repo('.')
+        patch = repo.git.diff("HEAD~1")
+        enabled_projects = ';'.join(cp.choose_projects(patch))
+        if enabled_projects is None or len(enabled_projects) == 0:
+            enabled_projects = 'all'
+        return enabled_projects
+    return projects
+
+
+def _create_env(config: Configuration):
+    """Generate the environment variables for cmake."""
+    env = os.environ.copy()
+
+    for key, value in config.environment.items():
+        env[key] = value
+
+    # TODO: set ENV on windows:
+    # load Vistual Studio environment variables
+    # Invoke-CmdScript "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=amd64 -host_arch=amd64
+
+    return env
+
+
+def _create_args(config: Configuration, llvm_enable_projects: str) -> List[str]:
+    """Generate the command line arguments for cmake."""
+    arguments = [
+        os.path.join('..', 'llvm'),
+        '-D LLVM_ENABLE_PROJECTS="{}"'.format(llvm_enable_projects),
+    ]
+    arguments.extend(config.general_cmake_arguments)
+    arguments.extend(config.specific_cmake_arguments)
+
+    # enable ccache if the path is set in the environment
+    if 'CCACHE_PATH' in os.environ:
+        arguments.extend([
+            '-D LLVM_CCACHE_BUILD=ON',
+            '-D LLVM_CCACHE_DIR={}'.format(os.environ['CCACHE_PATH']),
+            '-D LLVM_CCACHE_MAXSIZE=20G',
+        ])
+    return arguments
+
+
+def run_cmake(projects: str, repo_path: str, config_file_path: str = None):
+    """Use cmake to configure the project.
+
+    This version works on all operating systems.
+    """
+    if config_file_path is None:
+        script_dir = os.path.dirname(__file__)
+        config_file_path = os.path.join(script_dir, 'run_cmake_config.yaml')
+    config = Configuration(config_file_path)
+
+    build_dir = os.path.abspath(os.path.join(repo_path, 'build'))
+    if os.path.exists(build_dir):
+        shutil.rmtree(build_dir)
+    os.makedirs(build_dir)
+
+    env = _create_env(config)
+    llvm_enable_projects = _select_projects(config, projects, repo_path)
+    arguments = _create_args(config, llvm_enable_projects)
+    cmd = 'cmake ' + ' '.join(arguments)
+    print('Running Cmake:\n{}'.format(cmd))
+    subprocess.check_call(cmd, env=env, shell=True, cwd=build_dir)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run CMake for LLVM.')
+    parser.add_argument('projects', type=str, nargs='?', default='default')
+    parser.add_argument('repo_path', type=str, nargs='?', default=os.getcwd())
+    args = parser.parse_args()
+    run_cmake(args.projects, args.repo_path)
+
