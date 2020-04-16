1542f7d8aebbcad93464f047e1f648819dd4da88
Christian Kühnel <kuhnel@google.com>
2020-04-16 11:15:40 +0200

    using unlink

--- scripts/run_cmake.py
+++ scripts/run_cmake.py
@@ -163,7 +163,7 @@ def secure_delete(path: str):
         if not os.path.exists(name):
             return
         os.chmod(name, stat.S_IWRITE)
-        os.remove(name)
+        os.unlink(name)
 
     shutil.rmtree(path, onerror=del_rw)
 
