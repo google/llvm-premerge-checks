c350101a23037d39e99f3bd0878d726bc772c6f2
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2020-10-02 16:18:33 +0200

    Allow projects to define custom steps
    Now it's possible to allow sub-projects to define own checks and skip
    "generic" ones.
    
    To properly accomodate affected projects that might not have special
    treatment we:
    
    1. extend the set of affected projecs with dependent (e.g. add 'libc' if
    'clang' was modified)
    
    2. add custom steps for projects that define own workflow. At the moment
    it's only libcxx and it has a custom trigger pipeline so it's noop.
    
    3. add dependent projects and run generic check on them.
    
    To illustrate: imagine that we have a dependency graph:
    
    llvm -> clang -> openmp
    
    and only clang was modified in a diff; also clang defines own checks.
    Thus list of affected projects will be [clang, openmp].
    After adding custom checks and removing their projecst: [openmp].
    After adding dependencies: [llvm, clang, openmp]. Generic linux /
    windows checks will be run on thouse 3 projects.
    So as you can see in some scenarios projects with custom checks will
    still go through generic checks.
    
    Note that clang-format and clang-tidy checks are run only for "generic"
    checks at the moment.

---
+++ scripts/steps.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+# Copyright 2020 Google LLC
+#
+# Licensed under the the Apache License v2.0 with LLVM Exceptions (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://llvm.org/LICENSE.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import os
+from typing import List
+
+
+def libcxx():
+    # TODO: read steps from the YAML file from the repo.
+    return []
+
+
+def generic_linux(projects: str, check_diff: bool) -> List:
+    if os.getenv('ph_skip_linux') is not None:
+        return []
+    scripts_refspec = os.getenv("ph_scripts_refspec", "master")
+    no_cache = os.getenv('ph_no_cache') is not None
+    filter_output = '--filter-output' if os.getenv('ph_no_filter_output') is None else ''
+    log_level = os.getenv('ph_log_level', 'WARNING')
+    linux_agents = {'queue': 'linux'}
+    t = os.getenv('ph_linux_agents')
+    if t is not None:
+        linux_agents = json.loads(t)
+    commands = [
+        'set -euo pipefail',
+        'ccache --clear' if no_cache else '',
+        'ccache --zero-stats',
+        'ccache --show-config',
+        'mkdir -p artifacts',
+        'dpkg -l >> artifacts/packages.txt',
+        # Clone scripts.
+        'export SRC=${BUILDKITE_BUILD_PATH}/llvm-premerge-checks',
+        'rm -rf ${SRC}',
+        'git clone --depth 1 https://github.com/google/llvm-premerge-checks.git "${SRC}"',
+        'cd ${SRC}',
+        f'git fetch origin "{scripts_refspec}":x',
+        'git checkout x',
+        'echo "llvm-premerge-checks commit"',
+        'git rev-parse HEAD',
+        'cd "$BUILDKITE_BUILD_CHECKOUT_PATH"',
+        'set +e',
+    ]
+    if check_diff:
+        commands.extend([
+            # Add link in review to the build.
+            '${SRC}/scripts/add_phabricator_artifact.py '
+            '--phid="$ph_target_phid" '
+            '--url="$BUILDKITE_BUILD_URL" '
+            '--name="Buildkite build"' if check_diff else '',
+            '${SRC}/scripts/premerge_checks.py --check-clang-format --check-clang-tidy '
+            f'--projects="{projects}" --log-level={log_level} {filter_output}',
+        ])
+    else:
+        commands.extend([
+            f'${{SRC}}/scripts/premerge_checks.py --projects="{projects}" --log-level={log_level} {filter_output}'
+        ])
+    commands.extend([
+        'EXIT_STATUS=\\$?',
+        'echo "--- ccache stats"',
+        'ccache --print-stats',
+        'ccache --show-stats',
+        'exit \\$EXIT_STATUS',
+    ])
+
+    linux_buld_step = {
+        'label': ':linux: build and test linux',
+        'key': 'linux',
+        'commands': commands,
+        'artifact_paths': ['artifacts/**/*', '*_result.json'],
+        'agents': linux_agents,
+        'timeout_in_minutes': 120,
+        'retry': {'automatic': [
+            {'exit_status': -1, 'limit': 2},  # Agent lost
+            {'exit_status': 255, 'limit': 2},  # Forced agent shutdown
+        ]},
+    }
+    return [linux_buld_step]
+
+
+def generic_windows(projects: str) -> List:
+    # TODO: windows builds are temporary disabled #243
+    return []
+    if os.getenv('ph_skip_windows') is not None:
+        return []
+    scripts_refspec = os.getenv("ph_scripts_refspec", "master")
+    no_cache = os.getenv('ph_no_cache') is not None
+    log_level = os.getenv('ph_log_level', 'WARNING')
+    filter_output = '--filter-output' if os.getenv('ph_no_filter_output') is None else ''
+    clear_sccache = 'powershell -command "sccache --stop-server; echo \\$env:SCCACHE_DIR; ' \
+                    'Remove-Item -Recurse -Force -ErrorAction Ignore \\$env:SCCACHE_DIR; ' \
+                    'sccache --start-server"'
+    win_agents = {'queue': 'windows'}
+    t = os.getenv('ph_windows_agents')
+    if t is not None:
+        win_agents = json.loads(t)
+    windows_buld_step = {
+        'label': ':windows: build and test windows',
+        'key': 'windows',
+        'commands': [
+            clear_sccache if no_cache else '',
+            'sccache --zero-stats',
+
+            # Clone scripts.
+            'set SRC=%BUILDKITE_BUILD_PATH%/llvm-premerge-checks',
+            'rm -rf %SRC%',
+            'git clone --depth 1 https://github.com/google/llvm-premerge-checks.git %SRC%',
+            'cd %SRC%',
+            f'git fetch origin "{scripts_refspec}":x',
+            'git checkout x',
+            'echo llvm-premerge-checks commit:',
+            'git rev-parse HEAD',
+            'cd %BUILDKITE_BUILD_CHECKOUT_PATH%',
+
+            'powershell -command "'
+            f'%SRC%/scripts/premerge_checks.py --projects=\'{projects}\' --log-level={log_level} {filter_output}; '
+            '\\$exit=\\$?;'
+            'sccache --show-stats;'
+            'if (\\$exit) {'
+            '  echo success;'
+            '  exit 0; } '
+            'else {'
+            '  echo failure;'
+            '  exit 1;'
+            '}"',
+        ],
+        'artifact_paths': ['artifacts/**/*', '*_result.json'],
+        'agents': win_agents,
+        'timeout_in_minutes': 120,
+        'retry': {'automatic': [
+            {'exit_status': -1, 'limit': 2},  # Agent lost
+            {'exit_status': 255, 'limit': 2},  # Forced agent shutdown
+        ]},
+    }
+    return [windows_buld_step]
