c960512af9e1b5e357dd82ff9f8eb1a456a67dff
Grigory <movsic@gmail.com>
2022-11-23 12:21:10 +0100

    Add terraform configuration

---
+++ terraform/variables.tf
@@ -0,0 +1,82 @@
+variable "project-id" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "europe-west3"
+}
+
+variable "zone" {
+  type    = string
+  default = "europe-west3-c"
+}
+
+variable "subnetwork-main-cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "master-cidr" {
+  type    = string
+  default = "10.1.0.0/28"
+}
+
+variable "subnetwork-pods-cidr" {
+  type    = string
+  default = "10.2.0.0/16"
+}
+
+variable "subnetwork-services-cidr" {
+  type    = string
+  default = "10.3.0.0/16"
+}
+
+variable "linux-agents-machine-type" {
+  type    = string
+  default = "e2-standard-32"
+}
+
+variable "linux-agents-count" {
+  type    = number
+  default = 6
+}
+
+variable "linux-agents-build-queue" {
+  type    = string
+  default = "linux"
+}
+
+variable "linux-agents-cpu-request" {
+  type    = string
+  default = "30"
+}
+
+variable "linux-agents-mem-request" {
+  type    = string
+  default = "80Gi"
+}
+
+variable "buildkite-api-token-readonly" {
+  type = string
+}
+
+variable "buildkite-agent-token" {
+  type = string
+}
+
+variable "conduit-api-token" {
+  type = string
+}
+
+variable "git-id-rsa" {
+  type = string
+}
+
+variable "id-rsa-pub" {
+  type = string
+}
+
+variable "git-known-hosts" {
+  type = string
+}
