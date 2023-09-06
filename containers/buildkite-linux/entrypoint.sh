55177548b0663c99ce9bc247239068c066668245
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2023-09-06 14:39:09 +0200

    sccache on linux + deployment sizes

--- containers/buildkite-linux/entrypoint.sh
+++ containers/buildkite-linux/entrypoint.sh
@@ -26,6 +26,14 @@ export CCACHE_MAXSIZE=20G
 mkdir -p "${CCACHE_DIR}"
 chown -R ${USER}:${USER} "${CCACHE_DIR}"
 
+export SCCACHE_DIR="$BUILDKITE_BUILD_PATH/sccache"
+export SCCACHE_IDLE_TIMEOUT="0"
+rm -rf "$SCCACHE_DIR"
+mkdir -p "${SCCACHE_DIR}"
+chown -R ${USER}:${USER} "${SCCACHE_DIR}"
+chmod oug+rw "${SCCACHE_DIR}"
+gosu "$USER" bash -c 'env; whoami; SCCACHE_DIR="${SCCACHE_DIR}" SCCACHE_IDLE_TIMEOUT=0 SCCACHE_CACHE_SIZE=20G sccache --start-server'
+
 # /mnt/ssh should contain known_hosts, id_rsa and id_rsa.pub .
 mkdir -p /var/lib/buildkite-agent/.ssh
 if [ -d /mnt/ssh ]; then
@@ -36,4 +44,5 @@ if [ -d /mnt/ssh ]; then
 else
   echo "/mnt/ssh is not mounted"
 fi
+# Run with tini to correctly pass exit codes.
 exec /usr/bin/tini -g -- $@
