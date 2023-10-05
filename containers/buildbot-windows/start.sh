c3069b1cf6189c2b66254d8db644ed309b217ad4
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2023-10-05 13:37:06 +0000

    windows builder for buildbot

--- containers/buildbot-windows/start.sh
+++ containers/buildbot-windows/start.sh
@@ -1,15 +1,5 @@
 # Checkout path is set to a fixed short value (e.g. c:\ws\src) to keep paths
 # short as many tools break on Windows with paths longer than 250.
 
-: ${WORKDIR:=/c/ws}
-echo "WORKDIR $WORKDIR"
-mkdir -p "$WORKDIR"
-
-SCCACHE_DIR="$WORKDIR/sccache"
-SCCACHE_IDLE_TIMEOUT="0"
-SCCACHE_CACHE_SIZE=20G
-sccache --start-server
-
-whoami
-env
-tail -f
+echo "start buildbot"
+CC=cl CXX=cl LD=link buildbot-worker start /c/ws/buildbot
