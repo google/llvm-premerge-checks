97bd45070b3625daf2a481be0bbdeda0e18056e4
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2023-10-05 13:37:06 +0000

    windows builder for buildbot

--- containers/buildbot-windows/entrypoint.sh
+++ containers/buildbot-windows/entrypoint.sh
@@ -1,5 +1,17 @@
 echo "buildbot windows entrypoint"
 
+: ${WORKDIR:=/c/ws}
+echo "WORKDIR $WORKDIR"
+mkdir -p "$WORKDIR"
+echo "starting sccache"
+export SCCACHE_DIR="$WORKDIR/sccache"
+export SCCACHE_IDLE_TIMEOUT="0"
+export SCCACHE_CACHE_SIZE=20G
+sccache --start-server
+sccache --show-stats
+
+echo "configure buildbot"
 mkdir -p /c/ws/buildbbot
+buildbot-worker create-worker /c/ws/buildbot $BUILDBOT_ADDRESS $BUILDBOT_NAME $BUILDBOT_PASSWORD
 
 $@
